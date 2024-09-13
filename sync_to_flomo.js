21234d115b930378c45a1a13965ca21869070660
quantekk <93025716+quantekk@users.noreply.github.com>
2024-09-13 19:20:02 +0800

    Add more sites

--- sync_to_flomo.js
+++ sync_to_flomo.js
@@ -8,6 +8,7 @@
 // @match        https://www.4d4y.com/*
 // @match        https://*.zhihu.com/*
 // @match        https://mp.weixin.qq.com/*
+// @match        https://www.jisilu.cn/*
 // @connect      flomoapp.com
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
@@ -15,21 +16,16 @@
 // @license      MIT
 // ==/UserScript==
 
-
 (function () {
     "use strict";
-
     var flomoFullContent = "";
-
     function createFlomoButton(flomoApi) {
-
         const img = document.createElement("img");
         img.style.cursor = "pointer";
         img.style.width = "32px";
         img.style.height = "32px";
         img.style.border = "2px solid #30CF79";
         img.style.borderRadius = "8px";
-
         img.onclick = function () {
             GM_xmlhttpRequest({
                 method: "POST",
@@ -50,7 +46,6 @@
                 img.style.height = "32px";
             }, 200);
         };
-
         GM_xmlhttpRequest({
             method: "GET",
             url: "https://flomoapp.com/images/logo-192x192.png",
@@ -59,7 +54,6 @@
                 img.src = URL.createObjectURL(response.response);
             },
         });
-
         const div = document.createElement("div");
         div.appendChild(img);
         div.style.display = "flex";
@@ -68,10 +62,8 @@
         div.style.width = "36px";
         div.style = "top:90%;right:50%;position:fixed;"
         document.body.appendChild(div);
-
         return div;
     }
-
     function fetchFlomoApi() {
         let flomoApi = GM_getValue("flomo_api");
         if (!flomoApi) {
@@ -86,10 +78,8 @@
         }
         return flomoApi;
     }
-
     function processXueqiu() {
         console.log("try Xueqiu process");
-
         let postDocTitle = document.querySelector('#app > div.container.article__container > article > h1');
         let postTitle = '';
         if (postDocTitle.textContent.length>0) {
@@ -99,41 +89,30 @@
         let postAuthorLink = postDoc.children[0].textContent;
         postDoc.removeChild(postDoc.children[0]);
         let postContent = postDoc.innerHTML.replace(/<br>/g, "\n\r").replace(/<\/p>/g, "\n\r").replace(/<[^>]*>?/gm, '');
-
         console.log(postTitle);
         console.log(postAuthorLink);
         console.log(postContent);
-
         flomoFullContent = '#雪球 ' + postTitle + postContent + '\n\r' + postAuthorLink;
-
         return true;
     }
-
     function processSelection() {
         console.log("try selection process");
-
         let postLink = window.location.href;
         console.log(postLink);
-
         flomoFullContent = '#选择 '  + '\n\r' + postLink;
-
         document.addEventListener("selectionchange",event=>{
             let postTitle = document.title;
             let selection = document.getSelection ? document.getSelection().toString() :  document.selection.createRange().toString() ;
             flomoFullContent = '#网站 ' + postTitle +'\n\r'+ selection + '\n\r' + postLink;
         })
-
         return true;
     }
-
     function setup(){
         const flomoApi = fetchFlomoApi();
         if (!flomoApi) {
             return true;
         }
-
         const flomoButton = createFlomoButton(flomoApi);
-
         var href = window.location.host;
         console.log(href);
         if (href == 'xueqiu.com'){
@@ -142,7 +121,5 @@
             processSelection();
         }
     }
-
     setup();
-
 })();
